300eb5aa97ebd594f836b5ed50af34ca2d553735
limjaehee <rhak39@gmail.com>
2022-11-23 21:15:44 +0900

    스터디 작업~

---
+++ components/ClassItem.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="class-item">
+    <h2>나는 {{ item.name }}</h2>
+    <p>나는 {{ item.message }}</p>
+    <div v-if="item.user">
+      <span>{{ item.user.id }}</span>
+    </div>
+    <div v-if="item.published_lecture">
+      <div v-for="lecture in item.published_lecture" :key="lecture.i">
+        {{ lecture.id }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: {},
+    },
+  },
+  data() {
+    return {};
+  },
+  mounted() {
+    console.log(this.item);
+
+    if (this.item.user?.id) {
+      console.log("ㅎ하");
+    }
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.class-item {
+  padding: 20px;
+  border: 1px solid #ddd;
+  margin-bottom: 20px;
+  width: fit-content;
+  box-sizing: border-box;
+  background: green;
+  color: #fff;
+
+  &__user {
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,56 @@
-<template></template>
+<template>
+  <div>
+    <input type="text" v-model="inputValue" />
+    <ClassItem v-for="item in classList" :key="item.i" :item="item"></ClassItem>
+  </div>
+</template>
 
 <script>
+import ClassItem from "@/components/ClassItem";
+import value from "@/mixin/value.js";
+
 export default {
-  components: {},
+  mixin: [value],
+  components: {
+    ClassItem,
+  },
+  data() {
+    return {
+      inputValue: null,
+      classList: [
+        {
+          name: "kiki",
+          message: "hello world",
+          user: {
+            id: 20,
+            type: 1,
+            price: 2000,
+          },
+          published_lecture: [
+            {
+              id: 1,
+            },
+            {
+              id: 2,
+            },
+          ],
+        },
+        {
+          name: "어쩌구",
+          message: "hello!!",
+        },
+      ],
+    };
+  },
+  watch: {
+    inputValue(value) {
+      console.log(value);
+    },
+  },
+  methods: {},
+  mounted() {
+    this.formattedDate();
+  },
 };
 </script>
 
